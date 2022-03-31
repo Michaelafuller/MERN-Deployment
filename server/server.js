178fda88bbc316d8664ebe74688fc38a7e956334
Michael <Michaellovesellen@gmail.com>
2022-03-31 17:10:59 -0600

    solving merge conflicts

--- server/server.js
+++ server/server.js
@@ -2,15 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-const cors = require('cors')
-const DB = "lemonade_DB";
-
-app.use(cors(), express.json(), express.urlencoded({ extended: true }));
-require('./config/mongoose.config')(DB);
-
-require('./routes/Transaction.routes')(app);
-require('./routes/Stand.routes')(app);
-
 const cors = require('cors');
 const DB = "lemonade_DB";
 
