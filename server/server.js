b929cc8a7d482efdad0818a435a2446e8a3fe5bf
Michael <Michaellovesellen@gmail.com>
2022-03-30 21:05:57 -0600

    fixed merge conflict

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-const cors = require('cors')
+const cors = require('cors');
+const DB = "lemonade_DB";
 
 app.use(cors(), express.json(), express.urlencoded({ extended: true }));
-// require('./routes/todo.route')(app);
-// require('./config/mongoose.config');
+require('./routes/Stand.routes')(app);
+require('./routes/Transaction.routes')(app);
+require('./config/mongoose.config')(DB);
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
